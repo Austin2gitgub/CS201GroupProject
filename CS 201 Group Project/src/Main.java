eb6120aafad68302e778a470f168e6975d5d9b73
Austin-GitHub <84061326+Austin2gitgub@users.noreply.github.com>
2022-11-24 21:03:56 -0600

    Main Menu Added

--- CS 201 Group Project/src/Main.java
+++ CS 201 Group Project/src/Main.java
@@ -5,6 +5,7 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
@@ -23,7 +24,7 @@ public class Main {
                 ListArrayVar.add(line);
             }
 
-            //Cut the List and place it into the 2D Array
+            //Cut the List and place it into a 2D Array
             var TwoDimdata = new String[ListArrayVar.size()][ListArrayVar.get(0).toString().split(",").length];
             for (var i = 0; i < TwoDimdata.length; i++) {
                 for (var j = 0; j < TwoDimdata[0].length; j++) {
@@ -39,6 +40,38 @@ public class Main {
 
             //Code to output all the rows in our 2D array
             //System.out.println(Arrays.deepToString(TwoDimdata).replace("], ", "]\n"));
+
+            //Main Menu
+            Scanner input = new Scanner(System.in);
+            System.out.println("\n"+"           Welcome to the product management dashboard\n");
+            System.out.println("1. Do you want to add an product? If so type '1'.");
+            System.out.println("2. Do you want to search an product? If so type '2'.");
+            System.out.println("3. Do you want to list an product based on category? If so type '3'.");
+            System.out.println("4. Do you want to exit the dashboard? If so type 'Exit'.");
+            String inputAction = input.next();
+            if(inputAction.equals("1")){
+
+                //AddItemInList();
+                //or
+                //Fill code for that here
+            } else if (inputAction.equals("2")) {
+
+                //SearchItemInList();
+                //or
+                //Fill code for that here
+            } else if (inputAction.equals("3")) {
+
+                //SearchItemByCategoryInList();
+                //or
+                //Fill code for that here
+            }
+            else if(inputAction.equals("Exit")||inputAction.equals("exit")){
+                System.out.println("Thank you for using our dashboard");
+            }
+            else{
+                System.out.println("Wrong Input!");
+            }
+
         }
         catch (FileNotFoundException e){
 
