47d5b7405b7be8320583ab9e23943c7cec6615c1
Austin-GitHub <84061326+Austin2gitgub@users.noreply.github.com>
2022-11-25 14:29:27 -0600

    Add Product Feature Added

--- CS 201 Group Project/src/Main.java
+++ CS 201 Group Project/src/Main.java
@@ -10,6 +10,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         try {//Enclosing inside a try catch block to catch exceptions
+            int counter =21;
 
             //Reading the File
             String txtFilePath = "C:\\Users\\Austin\\Downloads\\Mock_data.txt";
@@ -32,6 +33,7 @@ public class Main {
                     TwoDimdata[i][j] = arr[j];
                 }
             }
+
 // Code for outputing 1st(Index wise) row of our 2D Array
 //                for(int a=0;a<6;a++){
 //
@@ -42,35 +44,94 @@ public class Main {
             //System.out.println(Arrays.deepToString(TwoDimdata).replace("], ", "]\n"));
 
             //Main Menu
-            Scanner input = new Scanner(System.in);
-            System.out.println("\n"+"           Welcome to the product management dashboard\n");
-            System.out.println("1. Do you want to add an product? If so type '1'.");
-            System.out.println("2. Do you want to search an product? If so type '2'.");
-            System.out.println("3. Do you want to list an product based on category? If so type '3'.");
-            System.out.println("4. Do you want to exit the dashboard? If so type 'Exit'.");
-            String inputAction = input.next();
-            if(inputAction.equals("1")){
+            boolean mainMenuRepeatCheck = false;
+            do {//Creating a Do while in-order to repeat 'do' when the input is not valid
 
-                //AddItemInList();
-                //or
-                //Fill code for that here
-            } else if (inputAction.equals("2")) {
+                //Getting input from the user for Main Menu
+                Scanner input = new Scanner(System.in);
+                System.out.println("\n" + "           Welcome to the product management dashboard\n");
+                System.out.println("1. Do you want to add an product? If so type '1'.");
+                System.out.println("2. Do you want to search an product? If so type '2'.");
+                System.out.println("3. Do you want to list an product based on category? If so type '3'.");
+                System.out.println("4. Do you want to exit the dashboard? If so type 'Exit'.");
 
-                //SearchItemInList();
-                //or
-                //Fill code for that here
-            } else if (inputAction.equals("3")) {
+                String inputAction = input.next();
 
-                //SearchItemByCategoryInList();
-                //or
-                //Fill code for that here
+                //Validations to assign to the correct code operation ex: add product, etc.
+                if (inputAction.equals("1")) {
+                    boolean addProductRepeatCheck = false;
+                    do {//Creating a Do while in-order to repeat 'do' when the input is not valid
+
+                        //Getting the input from the user for adding the product
+                        System.out.println("For 2 words or more give a '_' in between ex: Whole_Pepper");
+                        System.out.println("What is the name?");
+                        String nameInput = input.next();
+                        input.nextLine();
+                        System.out.println("What is the Category?");
+                        String categoryInput = input.next();
+                        System.out.println("What is the Type?");
+                        String typeInput = input.next();
+                        System.out.println("What is the Price(ex: $0.50)?");
+                        String priceInput = input.next();
+                        System.out.println("What is the Expiration(in days)?");
+                        String expirationInput = input.next();
+                        System.out.println("What is the Status(In-Store or Sold-Out)?");
+                        String statusInput = input.next();
+
+                        //Validation for null Input
+                        if (nameInput.length() < 1 || categoryInput.length() < 1 || typeInput.length() < 1 || priceInput.length() < 1 || expirationInput.length() < 1 || statusInput.length() < 1) {
+                            System.out.println("No Input or Partial Input, please type all inputs fully again!");
+                            addProductRepeatCheck=true; //condition check for do while
+                        } else {
+                            counter++;//adding all the product values ie. name, etc. to below the last existing data
+                            TwoDimdata[counter-1][0] = nameInput;
+                            TwoDimdata[counter-1][1] = categoryInput;
+                            TwoDimdata[counter-1][2] = typeInput;
+                            TwoDimdata[counter-1][3] = priceInput;
+                            TwoDimdata[counter-1][4] = expirationInput;
+                            TwoDimdata[counter-1][5] = statusInput;
+
+                            System.out.println("The product is successfully added");
+
+                            System.out.println("If you want to add another product, type 1");
+                            System.out.println("If you want to go to main menu, type 2");
+                            boolean function1FinalDecisionRepeatCheck = false;
+                            do {//Creating a Do while in-order to repeat 'do' when the input is not valid
+                                String function1FinalDecision = input.next();
+                                if (function1FinalDecision.equals("1")) {//Condition for changing do while
+                                    addProductRepeatCheck = true;
+                                } else if (function1FinalDecision.equals("2")) {//Condition for changing do while
+                                    mainMenuRepeatCheck = true;
+                                } else {//Condition for changing do while
+                                    function1FinalDecisionRepeatCheck = true;
+                                    System.out.println("Wrong Input, please try again!");
+                                }
+                            }
+                            while(function1FinalDecisionRepeatCheck==true);
+                        }
+                    }
+                    while (addProductRepeatCheck==true);
+
+
+                } else if (inputAction.equals("2")) {
+                    System.out.println(Arrays.deepToString(TwoDimdata).replace("], ", "]\n"));
+                    //SearchItemInList();
+                    //or
+                    //Fill code for that here
+                } else if (inputAction.equals("3")) {
+
+                    //SearchItemByCategoryInList();
+                    //or
+                    //Fill code for that here
+                } else if (inputAction.equals("Exit") || inputAction.equals("exit")) {
+                    mainMenuRepeatCheck =false;
+                    System.out.println("Thank you for using our dashboard");
+                } else {
+                    mainMenuRepeatCheck =true;
+                    System.out.println("Wrong Input, please try again!");
+                }
             }
-            else if(inputAction.equals("Exit")||inputAction.equals("exit")){
-                System.out.println("Thank you for using our dashboard");
-            }
-            else{
-                System.out.println("Wrong Input!");
-            }
+            while (mainMenuRepeatCheck==true);
 
         }
         catch (FileNotFoundException e){
